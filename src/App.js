c637d8dd603f5dc8f4219cc68862b4150c94cc07
Vlad7621 <vladisalv20017621@gmail.com>
2022-01-25 16:43:00 +0200

    3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './public/Home/Home';
